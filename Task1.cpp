df10711b57063c9cbace2197126820b4b98a12e6
怜矢 黒岩 <satoya.kuroiwa@gmail.com>
2024-10-19 16:16:48 +0900

    プロジェクト ファイルを追加します。

---
+++ Task1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int money = 0;
+	cout << "‚¨”N‹Ê‚ð‚à‚ç‚Á‚½‹àŠz‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B > ";
+	cin >> money;
+	cout << "‚ ‚È‚½‚ª‚à‚ç‚Á‚½Šz‚ÍA\\" << money << "‚Å‚·B";
+}
